a35a0b16449503f8a2f4bd7a61ab63a228b844cd
Jackson Quintero <codigoarchivo@gmail.com>
2023-07-09 21:08:27 -0400

    create model

---
+++ src/components/model.jsx
@@ -0,0 +1,26 @@
+import { useRef } from 'react';
+import { Window } from './window';
+import { Door } from './door';
+import { Barn } from './barn';
+// Barn
+const Model = (props) => {
+  // Instantiate a exporter
+  const group = useRef();
+
+  return (
+    <group ref={group} {...props} dispose={null}>
+      {/* window */}
+      <Window />
+      {/* door */}
+      <Door materials={props.materials} />
+      {/* barn */}
+      <Barn
+       materials={props.materials}
+       nodes={props.nodes}
+       selectData={props.selectData}
+      />
+    </group>
+  );
+};
+
+export default Model;
